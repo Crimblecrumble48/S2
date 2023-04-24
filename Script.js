8a03126de8beb1adbae2f8f6ff49717a834bd04f
Simon Harrison <simon.harrison22@bathspa.ac.uk>
2023-04-24 12:56:52 +0100

    Style to block overlay box

--- Script.js
+++
@@ -1,7 +0,0 @@
-const boxes = document.querySelectorAll(".box");
-
-		boxes.forEach((box) => {
-			box.addEventListener("mouseleave", () => {
-				box.querySelector(".overlay").style.transform = "translateY(150%)";
-			});
-		});
