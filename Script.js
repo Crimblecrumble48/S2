b8e30be29ea8a6caf7b405e07ece6d4ffc67ed0e
Simon Harrison <simon.harrison22@bathspa.ac.uk>
2023-05-12 12:02:56 +0100

    Script add for Blogs

---
+++ Script.js
@@ -0,0 +1,19 @@
+const Blog = document.getElementById('BlogInputs')
+
+function BlogEntry(input){
+
+    console.log(input)
+
+    if(input === 1){
+        document.getElementById('a1').style.display = "block"
+        document.getElementById('a2').style.display = "none"
+    }
+
+    else if(input === 2){
+        document.getElementById('a2').style.display = "block"
+        document.getElementById('a1').style.display = "none"
+    }
+
+
+
+}
