c35f7a980bc3dbbfe73878f1adff0292df51c968
Simon Harrison <simon.harrison22@bathspa.ac.uk>
2023-05-19 14:46:45 +0100

    JS edit for button addition

--- Script.js
+++ Script.js
@@ -6,6 +6,7 @@ function BlogEntry(input){
 
     if(input === 1){
         document.getElementById('a1').style.display = "block"
+        document.getElementById('a3').style.display = "none"
         document.getElementById('a2').style.display = "none"
     }
 
