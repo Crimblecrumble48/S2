be6dbe7a50571ff99ba29205b324a0269dcaad49
Simon Harrison <simon.harrison22@bathspa.ac.uk>
2023-05-22 12:15:10 +0100

    Blog Inputs added

--- Script.js
+++ Script.js
@@ -6,15 +6,21 @@ function BlogEntry(input){
 
     if(input === 1){
         document.getElementById('a1').style.display = "block"
-        document.getElementById('a3').style.display = "none"
         document.getElementById('a2').style.display = "none"
+        document.getElementById('a3').style.display = "none"
+        
     }
 
     else if(input === 2){
         document.getElementById('a2').style.display = "block"
         document.getElementById('a1').style.display = "none"
+        document.getElementById('a3').style.display = "none"
     }
-
+    else if(input === 3){
+        document.getElementById('a3').style.display = "block"
+        document.getElementById('a1').style.display = "none"
+        document.getElementById('a2').style.display = "none"
+    }
 
 
 }
