5dac5f1579206c13f3edbef5f730f7e44c250891
Simon Harrison <simon.harrison22@bathspa.ac.uk>
2023-05-22 13:24:22 +0100

    Start to add Google Maps

--- Script.js
+++ Script.js
@@ -8,19 +8,36 @@ function BlogEntry(input){
         document.getElementById('a1').style.display = "block"
         document.getElementById('a2').style.display = "none"
         document.getElementById('a3').style.display = "none"
-        
+        document.getElementById('a4').style.display = "none"
+        document.getElementById('a5').style.display = "none"
     }
 
     else if(input === 2){
         document.getElementById('a2').style.display = "block"
         document.getElementById('a1').style.display = "none"
         document.getElementById('a3').style.display = "none"
+        document.getElementById('a4').style.display = "none"
+        document.getElementById('a5').style.display = "none"
     }
     else if(input === 3){
         document.getElementById('a3').style.display = "block"
         document.getElementById('a1').style.display = "none"
         document.getElementById('a2').style.display = "none"
+        document.getElementById('a4').style.display = "none"
+        document.getElementById('a5').style.display = "none"
     }
-
-
+    else if(input === 4){
+        document.getElementById('a4').style.display = "block"
+        document.getElementById('a1').style.display = "none"
+        document.getElementById('a2').style.display = "none"
+        document.getElementById('a3').style.display = "none"
+        document.getElementById('a5').style.display = "none"
+    }
+    else if(input === 5){
+        document.getElementById('a5').style.display = "block"
+        document.getElementById('a1').style.display = "none"
+        document.getElementById('a2').style.display = "none"
+        document.getElementById('a3').style.display = "none"
+        document.getElementById('a4').style.display = "none"
+    }
 }
