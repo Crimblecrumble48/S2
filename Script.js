6bec6da8b09840207e6e673878b90954ef458749
Simon Harrison <simon.harrison22@bathspa.ac.uk>
2023-05-25 11:40:12 +0100

    Script complete - needs commentary

--- Script.js
+++ Script.js
@@ -43,8 +43,16 @@ function BlogEntry(input){
 }
 function myMap() {
     var mapProp= {
-      center:new google.maps.LatLng(51.438481,-0.937257),
-      zoom:15,
+      center:new google.maps.LatLng(51.43976121815247, -0.9359071119258889),
+      zoom:18,
+      mapTypeId: google.maps.MapTypeId.SATELLITE
+      
     };
     var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+    
+    new google.maps.Marker({ 
+        position: { lat: 51.43976121815247, lng: -0.9359071119258889},
+        map,
+        title: "CINN"
+    })
     }
